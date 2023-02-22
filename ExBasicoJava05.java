55447fe07dfde24f3db2ec67f2f2cfb81d2410b7
GabrielRamison <117987022+GabrielRamison@users.noreply.github.com>
2023-02-22 20:45:43 -0300

    Add files via upload

---
+++ ExBasicoJava05.java
@@ -0,0 +1,14 @@
+public class ExBasicoJava05{
+
+	public static void main(String args[]){
+		
+		int numero = 2;
+		
+		if (numero % 2 == 0)
+		System.out.println("O numero é par!");
+		else
+		System.out.println("O numero é impar!");
+
+	}
+
+}
